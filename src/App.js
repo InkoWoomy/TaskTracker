d7013f30840b3762ae006720cd743610f23d3442
InkoWoomy <yoda4115@gmail.com>
2024-02-12 15:56:23 -0800

    TaskTracker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Table from 'react-bootstrap/Table';
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import AddNewTask from './components/AddTaskModalComponent.js';
 
-function App() {
+function ToDo() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='bgTop'>
+    <Button variant='dark'>Add New Task</Button>
     </div>
+    
+    <div className='bgImg'>
+    
+    <Table striped bordered hover variant="dark">
+      <thead>
+        <tr>
+          
+          <th>To Do</th>
+          <th>In Progress</th>
+          <th>Completed</th>
+        </tr>
+      </thead>
+      <tbody>
+       
+
+      </tbody>
+    </Table>
+    </div>
+    </>
   );
 }
 
-export default App;
+export default ToDo;
